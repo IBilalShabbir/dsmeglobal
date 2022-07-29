bae87d62833952c800545c2eecb23155f41624d3
ahmadhaqofficial <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-07-29 03:58:35 -0700

    graphic design

--- src/screens/Graphicdesign.jsx
+++ src/screens/Graphicdesign.jsx
@@ -53,8 +53,17 @@ function Graphicdesign() {
           media companies and brands. It's working exceedingly well, and we're
           delivering some staggering.
         </div>
-        <div className="staff__talent__info" style={{ fontsize: 30 }}>
-          Want to create a lasting brand impact?
+        <div
+          className="staff__talent__info"
+          style={{ fontSize: "30px", fontWeight: "600" }}
+        >
+          Want to create a <span>lasting brand impact?</span>
+        </div>
+        <div
+          className="staff__talent__info"
+          style={{ width: "100%", marginBottom: "20px" }}
+        >
+          Get in touch with our graphic design services consultants.
         </div>
         <div className="staff__talent__features">
           {talentFeatures.map((feature) => (
@@ -80,56 +89,70 @@ function Graphicdesign() {
       <div className="graphic__design__essence__container__buttton">
         <a href="">Get started </a>
       </div>
-      <div className="graphic__design__essence__container__skills__and__image">
-        <div className="graphic__design__essence__container__image">
-          <img src={Graphic} alt="" />
-          <div className="graphic__design__essence__container__skills__all">
-            <div className="graphic__design__essence__container__skills__heading">
-              Skills
-            </div>
-            <div className="graphic__design__essence__container__skills__main">
-              <div className="graphic__design__essence__container__skills__one">
-                Graphic design
+      <div style={{ backgroundColor: "#242424", width: "100%" }}>
+        <div className="graphic__design__essence__container__skills__and__image">
+          <div className="graphic__design__essence__container__image">
+            <img src={Graphic} alt="" />
+            <div className="graphic__design__essence__container__skills__all">
+              <div className="graphic__design__essence__container__skills__heading">
+                Skills
               </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Product branding
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Bussines planing
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Photography
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Market research
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Strategic consulting
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Ecommerence consulting
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Web design
-              </div>
-              <div className="graphic__design__essence__container__skills__one">
-                Web development
+              <div className="graphic__design__essence__container__skills__main">
+                <div className="graphic__design__essence__container__skills__one">
+                  Graphic design
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Product branding
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Bussines planing
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Photography
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Market research
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Strategic consulting
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Ecommerence consulting
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Web design
+                </div>
+                <div className="graphic__design__essence__container__skills__one">
+                  Web development
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className="graphic__design__essence__container__we__offer">
+        <div className="graphic__design__essence__container__we__offer__heading">
+          WE OFFER CUSTOM PRICING BECAUSE EVERY GRAPHIC DESIGN SERVICE IS
+          DIFFERENT
+        </div>
+        <div className="graphic__design__essence__container__we__offer__content">
+          Through our years of experience, we’ve learned it’s best to start by
+          listening to your story. Then we review your existing brand and offer
+          you options from there. Below we’ve listed our most requested
+          packages, but each of these can be tweaked or we can give you a
+          completely custom quote.
+        </div>
+      </div>
+      <div className="graphic__design__essence__container__packages">
       <div className="graphic__design__essence__container__we__offer__heading">
-        WE OFFER CUSTOM PRICING BECAUSE EVERY GRAPHIC DESIGN SERVICE IS
-        DIFFERENT
+        LOGO & BRANDING DESIGN PACKAGES
       </div>
       <div className="graphic__design__essence__container__we__offer__content">
-        Through our years of experience, we’ve learned it’s best to start by
-        listening to your story. Then we review your existing brand and offer
-        you options from there. Below we’ve listed our most requested packages,
-        but each of these can be tweaked or we can give you a completely custom
-        quote.
+        DESIGN At DSME Global Links we begin every project with a conversation.
+        It’s important for us to spend time listening to your story before we
+        offer any solutions because every business is different. So before we
+        offer final estimates we spend time on the phone with you brain-storming
+        strategies and solutions that work best for you.
       </div>
       </div>
     </>
