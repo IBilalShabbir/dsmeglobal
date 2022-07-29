b5bc79a2620e29b7d3c9fcd1600e1b890c9c060a
ahmadhaqofficial <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-07-29 07:00:52 -0700

    changes

--- src/components/WhyBlockChainSection.jsx
+++ src/components/WhyBlockChainSection.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export function WhyBlockChainSection() {
   return (
     <div className="blockchain__whysection">
-      <div className="blockchain__whysection__heading">Why <span>Blockchain?</span> </div>
+      <div className="blockchain__whysection__heading">
+        Why <span>Blockchain?</span>{" "}
+      </div>
       <div className="blockchain__whysection__services">
         <div className="blockchain__whysection__servicecard">
           <div className="blockchain__whysection__servicecard__icon">
@@ -73,22 +75,25 @@ export function WhyBlockChainSection() {
           </div>
         </div>
         <div className="blockchain__whysection__servicecard">
-          <div className="blockchain__whysection__servicecard__icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="29.345"
-              height="35.214"
-              viewBox="0 0 29.345 35.214"
-            >
-              <g id="lock" transform="translate(-42.667)">
-                <path
-                  id="Path_36082"
-                  data-name="Path 36082"
-                  d="M67.61,12.36V10.271a10.271,10.271,0,1,0-20.541,0V12.36a7.336,7.336,0,0,0-4.4,6.714v8.8A7.345,7.345,0,0,0,50,35.214H64.676a7.345,7.345,0,0,0,7.336-7.336v-8.8A7.336,7.336,0,0,0,67.61,12.36Zm-8.8,12.583a1.467,1.467,0,1,1-2.934,0V22.009a1.467,1.467,0,1,1,2.934,0Zm5.869-13.205H50V10.271a7.336,7.336,0,1,1,14.672,0Z"
-                  fill="#189366"
-                />
-              </g>
-            </svg>
+          <div className="blockchain__whysection__servicecard__svg__content">
+            <div className="blockchain__whysection__servicecard__icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="29.345"
+                height="35.214"
+                viewBox="0 0 29.345 35.214"
+              >
+                <g id="lock" transform="translate(-42.667)">
+                  <path
+                    id="Path_36082"
+                    data-name="Path 36082"
+                    d="M67.61,12.36V10.271a10.271,10.271,0,1,0-20.541,0V12.36a7.336,7.336,0,0,0-4.4,6.714v8.8A7.345,7.345,0,0,0,50,35.214H64.676a7.345,7.345,0,0,0,7.336-7.336v-8.8A7.336,7.336,0,0,0,67.61,12.36Zm-8.8,12.583a1.467,1.467,0,1,1-2.934,0V22.009a1.467,1.467,0,1,1,2.934,0Zm5.869-13.205H50V10.271a7.336,7.336,0,1,1,14.672,0Z"
+                    fill="#189366"
+                  />
+                </g>
+              </svg>
+            </div>
+            Data Secure
           </div>
           <div className="blockchain__whysection__servicecard__heading">
             Make your Data Secure
@@ -99,20 +104,23 @@ export function WhyBlockChainSection() {
           </div>
         </div>
         <div className="blockchain__whysection__servicecard">
-          <div className="blockchain__whysection__servicecard__icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="35"
-              height="32"
-              viewBox="0 0 24 22.001"
-            >
-              <path
-                id="Subtraction_5"
-                data-name="Subtraction 5"
-                d="M17,22H7a1,1,0,0,1,0-2h4V18H5A5.009,5.009,0,0,1,.1,14H23.9A5.009,5.009,0,0,1,19,18H13v2h4a1,1,0,0,1,0,2Zm7-10H0V5A5.015,5.015,0,0,1,5,0H19a5.015,5.015,0,0,1,5,5v7ZM9.713,5.4a1.556,1.556,0,0,0-1.169.527L6,8.707a.7.7,0,0,0-.014.892.506.506,0,0,0,.783.018L9.32,6.824a.517.517,0,0,1,.782,0c.021.023.573.471.684.561a1.557,1.557,0,0,0,2.31-.032L15.8,4.363V6.638a.559.559,0,1,0,1.108,0V4.115a1.79,1.79,0,0,0-1.661-1.892H13.036a.6.6,0,0,0-.554.63.6.6,0,0,0,.554.631h1.974l-2.69,2.969a.518.518,0,0,1-.783,0c-.021-.023-.616-.506-.684-.561A1.541,1.541,0,0,0,9.713,5.4Z"
-                fill="#189366"
-              />
-            </svg>
+          <div className="blockchain__whysection__servicecard__svg__content">
+            <div className="blockchain__whysection__servicecard__icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="35"
+                height="32"
+                viewBox="0 0 24 22.001"
+              >
+                <path
+                  id="Subtraction_5"
+                  data-name="Subtraction 5"
+                  d="M17,22H7a1,1,0,0,1,0-2h4V18H5A5.009,5.009,0,0,1,.1,14H23.9A5.009,5.009,0,0,1,19,18H13v2h4a1,1,0,0,1,0,2Zm7-10H0V5A5.015,5.015,0,0,1,5,0H19a5.015,5.015,0,0,1,5,5v7ZM9.713,5.4a1.556,1.556,0,0,0-1.169.527L6,8.707a.7.7,0,0,0-.014.892.506.506,0,0,0,.783.018L9.32,6.824a.517.517,0,0,1,.782,0c.021.023.573.471.684.561a1.557,1.557,0,0,0,2.31-.032L15.8,4.363V6.638a.559.559,0,1,0,1.108,0V4.115a1.79,1.79,0,0,0-1.661-1.892H13.036a.6.6,0,0,0-.554.63.6.6,0,0,0,.554.631h1.974l-2.69,2.969a.518.518,0,0,1-.783,0c-.021-.023-.616-.506-.684-.561A1.541,1.541,0,0,0,9.713,5.4Z"
+                  fill="#189366"
+                />
+              </svg>
+            </div>
+            Transparency
           </div>
           <div className="blockchain__whysection__servicecard__heading">
             Ensure high Level of Transparency
@@ -123,32 +131,35 @@ export function WhyBlockChainSection() {
           </div>
         </div>
         <div className="blockchain__whysection__servicecard">
-          <div className="blockchain__whysection__servicecard__icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="26.569"
-              height="35.426"
-              viewBox="0 0 26.569 35.426"
-            >
-              <g id="user_1_" data-name="user (1)" transform="translate(-3)">
-                <path
-                  id="Path_36083"
-                  data-name="Path 36083"
-                  d="M22.252,14H10.317A7.326,7.326,0,0,0,3,21.317v7.444H29.569V21.317A7.326,7.326,0,0,0,22.252,14Z"
-                  transform="translate(0 6.665)"
-                  fill="#189366"
-                />
-                <circle
-                  id="Ellipse_639"
-                  data-name="Ellipse 639"
-                  cx="9"
-                  cy="9"
-                  r="9"
-                  transform="translate(7)"
-                  fill="#189366"
-                />
-              </g>
-            </svg>
+          <div className="blockchain__whysection__servicecard__svg__content">
+            <div className="blockchain__whysection__servicecard__icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26.569"
+                height="35.426"
+                viewBox="0 0 26.569 35.426"
+              >
+                <g id="user_1_" data-name="user (1)" transform="translate(-3)">
+                  <path
+                    id="Path_36083"
+                    data-name="Path 36083"
+                    d="M22.252,14H10.317A7.326,7.326,0,0,0,3,21.317v7.444H29.569V21.317A7.326,7.326,0,0,0,22.252,14Z"
+                    transform="translate(0 6.665)"
+                    fill="#189366"
+                  />
+                  <circle
+                    id="Ellipse_639"
+                    data-name="Ellipse 639"
+                    cx="9"
+                    cy="9"
+                    r="9"
+                    transform="translate(7)"
+                    fill="#189366"
+                  />
+                </g>
+              </svg>
+            </div>
+            Take control Info
           </div>
           <div className="blockchain__whysection__servicecard__heading">
             Take control of your Information
@@ -159,20 +170,23 @@ export function WhyBlockChainSection() {
           </div>
         </div>
         <div className="blockchain__whysection__servicecard">
-          <div className="blockchain__whysection__servicecard__icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="35"
-              viewBox="0 0 30 35"
-            >
-              <path
-                id="shield-check"
-                d="M26.871,3.121,17.474.074a1.541,1.541,0,0,0-.948,0l-9.4,3.046A7.3,7.3,0,0,0,2,10.038V17.5C2,28.529,15.8,34.62,16.391,34.874a1.539,1.539,0,0,0,1.218,0C18.2,34.62,32,28.529,32,17.5V10.038a7.3,7.3,0,0,0-5.128-6.917Zm-2.795,11.05L17.669,20.4a2.851,2.851,0,0,1-2,.806h-.05a2.845,2.845,0,0,1-2.017-.875l-3.459-3.5a1.432,1.432,0,0,1-.428-1.449,1.482,1.482,0,0,1,1.119-1.043,1.525,1.525,0,0,1,1.471.476l3.366,3.412L21.95,12.1a1.53,1.53,0,0,1,2.121,0,1.43,1.43,0,0,1,0,2.062Z"
-                transform="translate(-2)"
-                fill="#189366"
-              />
-            </svg>
+          <div className="blockchain__whysection__servicecard__svg__content">
+            <div className="blockchain__whysection__servicecard__icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="35"
+                viewBox="0 0 30 35"
+              >
+                <path
+                  id="shield-check"
+                  d="M26.871,3.121,17.474.074a1.541,1.541,0,0,0-.948,0l-9.4,3.046A7.3,7.3,0,0,0,2,10.038V17.5C2,28.529,15.8,34.62,16.391,34.874a1.539,1.539,0,0,0,1.218,0C18.2,34.62,32,28.529,32,17.5V10.038a7.3,7.3,0,0,0-5.128-6.917Zm-2.795,11.05L17.669,20.4a2.851,2.851,0,0,1-2,.806h-.05a2.845,2.845,0,0,1-2.017-.875l-3.459-3.5a1.432,1.432,0,0,1-.428-1.449,1.482,1.482,0,0,1,1.119-1.043,1.525,1.525,0,0,1,1.471.476l3.366,3.412L21.95,12.1a1.53,1.53,0,0,1,2.121,0,1.43,1.43,0,0,1,0,2.062Z"
+                  transform="translate(-2)"
+                  fill="#189366"
+                />
+              </svg>
+            </div>
+            Completeness
           </div>
           <div className="blockchain__whysection__servicecard__heading">
             Ensure Completeness of Data
