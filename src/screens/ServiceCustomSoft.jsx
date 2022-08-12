3b85f97d60ad2afd86b31b5622066712243d1593
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-08-12 02:05:46 -0700

    custom soft

--- src/screens/ServiceCustomSoft.jsx
+++ src/screens/ServiceCustomSoft.jsx
@@ -28,8 +28,8 @@ export default function ServiceCustomSoft() {
                 <div className="svg_coustsoft">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="34"
+                    width="40"
+                    height="40"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke=" #189366"
@@ -57,8 +57,8 @@ export default function ServiceCustomSoft() {
                 <div className="svg__two__coustm">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="34"
+                    width="20"
+                    height="20"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                     fill="none"
@@ -78,20 +78,7 @@ export default function ServiceCustomSoft() {
             <div className="customsoft_second_container">
               <div className="customsoft_second_container_svg">
                 <div className="svg__three_coustm">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="34"
-                    height="34"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-phone"
-                  >
-                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                  </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-smartphone"><rect x="5" y="2" width="14" height="20" rx="2" ry="2"></rect><line x1="12" y1="18" x2="12.01" y2="18"></line></svg>
                 </div>
                 <span>Mobile App Development</span>
               </div>
