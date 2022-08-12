61de9ffbce0dacbf5b7afb069902f8bb6aa4ef00
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-08-11 23:37:30 -0700

    svg changes

--- src/components/ServicesFeatureListItem.jsx
+++ src/components/ServicesFeatureListItem.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-export function ServicesFeatureListItem({ label, info }) {
+export function ServicesFeatureListItem({ label, info,icon }) {
   return (
     <div className="container__feature__col__entry">
+      <div className="container__feature__col__entry__heading">{icon}</div>
       <div className="container__feature__col__entry__heading">{label}</div>
       <div className="container__feature__col__entry__info">{info}</div>
     </div>
