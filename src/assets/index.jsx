d3c4433ff6f12994bc3d5d29ae6e57f5ea78af04
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-08-17 13:43:31 +0500

    home jumbotron finished

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,2 +1,6 @@
 export { default as logo } from "./logo.svg";
 export { default as homeBannerImage } from "./homeBannerImage.svg";
+export { default as homeBannerSvg1 } from "./homeBannerSvg1.svg";
+export { default as homeBannerSvg2 } from "./homeBannerSvg2.svg";
+export { default as homeBannerSvg3 } from "./homeBannerSvg3.svg";
+export { default as homeBannerSvg4 } from "./homeBannerSvg4.svg";
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { homeBannerImage } from "../assets";
+import {
+  homeBannerImage,
+  homeBannerSvg1,
+  homeBannerSvg2,
+  homeBannerSvg3,
+  homeBannerSvg4,
+} from "../assets";
 
 export default function Home() {
   return (
     <>
       <div className="home__jumbotron">
+        <img
+          src={homeBannerSvg1}
+          alt="homeBannerSvg1"
+          className="home__jumbotron__svg1"
+        />
+        <img
+          src={homeBannerSvg2}
+          alt="homeBannerSvg2"
+          className="home__jumbotron__svg2"
+        />
+        <img
+          src={homeBannerSvg3}
+          alt="homeBannerSvg3"
+          className="home__jumbotron__svg3"
+        />
+        <img
+          src={homeBannerSvg4}
+          alt="homeBannerSvg4"
+          className="home__jumbotron__svg4"
+        />
         <div className="home__jumbotron__content">
           <div className="home__jumbotron__content__left">
             <div className="home__jumbotron__content__left__heading heading">
