f040864e640e1f2e905998d1fdb269b2517d6896
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-08-17 13:03:44 +0500

    home jumbotron done

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { homeBannerImage } from "../assets";
 
 export default function Home() {
-  return <div style={{ height: "100vh", background: "#181818" }}>Home</div>;
+  return (
+    <>
+      <div className="home__jumbotron">
+        <div className="home__jumbotron__content">
+          <div className="home__jumbotron__content__left">
+            <div className="home__jumbotron__content__left__heading heading">
+              Quality <span>Products</span> <br /> You Really <span>Need</span>
+            </div>
+            <div className="home__jumbotron__content__left__info">
+              Whether you want to modernize your application portfolio broadly,
+              pursue specific opportunities in your industry, or optimize a
+              single technology, DSME brings together all the services with
+              leading back-office and industry-specific SaaS.
+            </div>
+            <NavLink
+              to="/quote"
+              className="home__jumbotron__content__left__button"
+            >
+              Get a quote
+            </NavLink>
+          </div>
+          <div className="home__jumbotron__content__right">
+            <img
+              src={homeBannerImage}
+              alt="homeBannerImage"
+              className="home__jumbotron__content__right__img"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
