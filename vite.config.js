6e5b1cf62a8c88adc83a905d7fbb1b6796058721
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-08-18 21:07:09 +0500

    dependencies added and updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), viteCompression(), ViteWebfontDownload()],
+});
