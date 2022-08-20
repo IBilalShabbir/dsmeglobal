3af4a650b7762ed73f5adcdc3e7659df9aa97b52
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-08-21 02:44:52 +0500

    optimizations and deployed

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression(), ViteWebfontDownload()],
+  plugins: [
+    react(),
+    viteCompression(),
+    ViteWebfontDownload(),
+    chunkSplitPlugin(),
+  ],
 });
