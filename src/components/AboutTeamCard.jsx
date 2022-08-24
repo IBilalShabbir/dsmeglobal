1dee090ae1ef2a787a33e75d83aaf4857318c4f9
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-08-25 00:31:10 +0500

    team integration done

--- src/components/AboutTeamCard.jsx
+++ src/components/AboutTeamCard.jsx
@@ -6,9 +6,7 @@ export function AboutTeamCard({ image, name, designation }) {
       <img
         src={
           import.meta.env.VITE_CLOUDNAIRY_API_URL.replace("q_50", "q_20") +
-          "v1658515904/dsme_team/" +
-          image +
-          ".webp"
+          image
         }
         alt={name}
         className="about__team__content__entry__img"
