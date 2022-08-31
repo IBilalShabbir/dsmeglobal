5c513873a275fa2bb4c286cef5cdfd1b3bdc904b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-08-31 14:33:33 +0500

    DedicatedTeamsPlans done

---
+++ src/components/DedicatedTeamsPlans.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export function DedicatedTeamsPlans() {
+  return (
+    <div className="dedicated__teams__plan">
+      <div className="dedicated__teams__plan__left">
+        <div className="dedicated__teams__plan__wrapper">
+          <div className="dedicated__teams__plan__heading heading">
+            Full Service Standalone Team
+          </div>
+          <div className="dedicated__teams__plan__info">
+            Not looking to build a complete software department but need tech
+            resources on demand.
+          </div>
+          <div className="dedicated__teams__plan__info">
+            Offers a complete setup where you get access to the best tech and
+            management talent (developers, UI/UX experts).
+          </div>
+        </div>
+        <div className="dedicated__teams__plan__content">
+          <div className="dedicated__teams__plan__content__heading heading">
+            Suited best when:
+          </div>
+          <div className="dedicated__teams__plan__content__info">
+            Developing new products without defocusing your core team (if
+            exists)
+          </div>
+          <div className="dedicated__teams__plan__content__info">
+            With evolving specifications based on feedbacks on features
+          </div>
+          <div className="dedicated__teams__plan__content__info">
+            Not looking to go through a long-lasting & expensive hiring process.
+          </div>
+        </div>
+      </div>
+      <div className="dedicated__teams__plan__right">
+        <div className="dedicated__teams__plan__wrapper">
+          <div className="dedicated__teams__plan__heading heading">
+            Extension of Existing Teams
+          </div>
+          <div className="dedicated__teams__plan__info">
+            Perfect solution when your current project initiatives need a few
+            additional dedicated specialized resources.
+          </div>
+          <div className="dedicated__teams__plan__info">
+            Our expert team members will consistently and cost effectively fill
+            the gaps in your project needs.
+          </div>
+        </div>
+        <div className="dedicated__teams__plan__content">
+          <div className="dedicated__teams__plan__content__heading heading">
+            Suited best when:
+          </div>
+          <div className="dedicated__teams__plan__content__info">
+            With their own in-house IT teams and established processes.
+          </div>
+          <div className="dedicated__teams__plan__content__info">
+            That require extra resources to satisfy ad-hoc demand.
+          </div>
+          <div className="dedicated__teams__plan__content__info">
+            Looking for resources experienced in latest technologies or
+            domain-specific expertise.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/screens/DedicatedTeams.jsx
+++ src/screens/DedicatedTeams.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { DedicatedTeamsWhy } from "../components/DedicatedTeamsWhy";
 import { DedicatedTeamsJumbotron } from "../components/DedicatedTeamsJumbotron";
 import { DedicatedTeamsAbout } from "../components/DedicatedTeamsAbout";
+import { DedicatedTeamsPlans } from "../components/DedicatedTeamsPlans";
 
 export default function DedicatedTeams() {
   return (
     <>
       <DedicatedTeamsJumbotron />
       <DedicatedTeamsAbout />
+      <DedicatedTeamsPlans />
       <DedicatedTeamsWhy />
     </>
   );
