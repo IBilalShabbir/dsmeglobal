a0fd965fb96c25d4ea573ccc41a291e6e7b8323c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-14 01:19:46 +0500

    BlockchainWhy done

--- src/screens/BlockchainConsulting.jsx
+++ src/screens/BlockchainConsulting.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BlockchainWhy } from "../components/BlockchainWhy";
 import { Jumbotron } from "../components/Jumbotron";
 
 export default function BlockchainConsulting() {
@@ -8,6 +9,7 @@ export default function BlockchainConsulting() {
         heading="DELIVERING ROBUST SOFTWARE SYSTEMS WITH BLOCKCHAIN"
         description="We help businesses leverage decentralized networks and software solutions built on blockchain technology and introduce a whole new level of efficiency, transparency, immutability, and automation into their processes. Our dedicated professionals with deep expertise in this revolutionary technology aim to provide efficient, integrated, and secure solutions for businesses representing different industries. Our wide range of services includes smart contracts, Defi solutions (Staking, liquidity, and farming), ICO, tokens, crypto exchanges, multichain blockchain development, gamification, NFTs, and more."
       />
+      <BlockchainWhy />
     </>
   );
 }
