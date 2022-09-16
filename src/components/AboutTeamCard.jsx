18737b3885cd4bc1a354423ce0a6ab2b48f8cda8
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-09-16 16:28:47 +0500

    deployed

--- src/components/AboutTeamCard.jsx
+++ src/components/AboutTeamCard.jsx
@@ -15,11 +15,11 @@ export function AboutTeamCard({ image, name, designation }) {
       />
       <div className="about__team__content__entry__content">
         <div className="about__team__content__entry__content__heading heading">
-          {name}
-        </div>
-        <div className="about__team__content__entry__content__info">
           {designation}
         </div>
+        {/* <div className="about__team__content__entry__content__info">
+          
+        </div> */}
       </div>
     </div>
   );
