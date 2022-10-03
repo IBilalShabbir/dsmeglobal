24d586ef41fc650dcc789476a897ccbc3c39adcb
Bilal Shabbir <102783392+IBilalShabbir@users.noreply.github.com>
2022-10-03 17:03:57 +0500

    Again done a lot

--- src/screens/FixedPrice.jsx
+++ src/screens/FixedPrice.jsx
@@ -6,6 +6,7 @@ import { FixedPriceWhyWork } from "../components/FixedPriceWhyWork";
 import { FixedPriceLinks } from "../components/FixedPriceLinks";
 import { blockChainGetStarted } from "../assets";
 import { FixedPriceProcess } from "../components/FixedPriceProcess";
+import { ReadyToGetStarted } from "../components/ReadyToGetStarted";
 
 export default function FixedPrice() {
   return (
@@ -30,129 +31,7 @@ export default function FixedPrice() {
       <FixedPriceWhyWork />
       <FixedPriceAllInclusivePricing />
       <FixedPriceProcess />
-      <div className="ready__to__get__started__section">
-        <div className="ready__to__get__started">
-          <div className="ready__to__get__started__heading__container">
-            <svg
-              width="420"
-              height="467"
-              viewBox="0 0 467 467"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M428.629 37.5429V428.629L37.5426 428.629V37.5429L428.629 37.5429Z"
-                stroke="url(#paint0_linear_68_1534)"
-                stroke-width="75"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_68_1534"
-                  x1="244"
-                  y1="456"
-                  x2="214.5"
-                  y2="98.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="#2ECC82" />
-                  <stop
-                    offset="0.552083"
-                    stop-color="#2ECC82"
-                    stop-opacity="0.26"
-                  />
-                  <stop offset="1" stop-color="#2ECC82" stop-opacity="0" />
-                </linearGradient>
-              </defs>
-            </svg>
-            <div className="ready__to__get__started__heading__container__overlay">
-              <div className="ready__to__get__started__heading heading">
-                Ready To Get Started?
-              </div>
-              <div className="ready__to__get__started__heading__two heading">
-                Let's talk
-              </div>
-              <button className="ready__to__get__started__overlay__lets__talk__btn">
-                Let's Talk
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="fixed__price__discovery">
-          <div className="fixed__price__discovery__container">
-            <svg
-              width="537"
-              height="537"
-              viewBox="0 0 537 537"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="268.638"
-                cy="268.639"
-                r="233.646"
-                transform="rotate(78.6594 268.638 268.639)"
-                stroke="url(#paint0_linear_68_1552)"
-                stroke-opacity="0.1"
-                stroke-width="69"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_68_1552"
-                  x1="268.638"
-                  y1="0.493103"
-                  x2="268.638"
-                  y2="536.785"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="#B6B6B6" />
-                  <stop offset="1" stop-color="#020202" stop-opacity="0" />
-                </linearGradient>
-              </defs>
-            </svg>
-            <div className="fixed__price__discovery__container__overlay">
-              <div className="fixed__price__discovery__container__overlay__heading heading">
-                Discovery Workshop
-              </div>
-              <div className="fixed__price__discovery__container__overlay__subheadings">
-                <div className="fixed__price__discovery__container__overlay__subheadings__one">
-                  2-Week Process
-                </div>
-                <div className="fixed__price__discovery__container__overlay__subheadings__two">
-                  To Ensure On-Time & On-Budget Delivery
-                </div>
-                <div className="fixed__price__discovery__container__overlay__text">
-                  During this workshop, our Product Strategists, Technical &
-                  UI/UX Experts collaborate with our clients, to
-                </div>
-                <div className="fixed__price__discovery__container__overlay__bottom__content">
-                  <div className="fixed__price__discovery__container__overlay__bottom__content__left">
-                    <div className="fixed__price__discovery__container__overlay__bottom__content__left__entry">
-                      Map & understand client vision
-                    </div>
-                    <div className="fixed__price__discovery__container__overlay__bottom__content__left__entry">
-                      Visualize the idea-
-                    </div>
-                    <div className="fixed__price__discovery__container__overlay__bottom__content__left__entry">
-                      Quantify business value (ROI)
-                    </div>
-                  </div>
-                  <div className="fixed__price__discovery__container__overlay__bottom__content__right">
-                    <div className="fixed__price__discovery__container__overlay__bottom__content__left__entry">
-                      Assess technical viability
-                    </div>
-                    <div className="fixed__price__discovery__container__overlay__bottom__content__left__entry">
-                      Define the scope for budgeting
-                    </div>
-                    <div className="fixed__price__discovery__container__overlay__bottom__content__left__entry">
-                      Develop a comprehensive plan
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ReadyToGetStarted />
     </>
   );
 }
